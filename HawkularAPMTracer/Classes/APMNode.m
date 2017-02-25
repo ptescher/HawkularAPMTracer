b62d159b6750b1154f3cda6a19f34c66e1f8b53d
Patrick Tescher <pat2man@gmail.com>
2017-02-24 17:36:33 -0800

    Remove type field

--- HawkularAPMTracer/Classes/APMNode.m
+++ HawkularAPMTracer/Classes/APMNode.m
@@ -45,7 +45,6 @@
 - (NSDictionary *)propertyDictionary {
     NSMutableDictionary *propertyDictionary = [NSMutableDictionary new];
     propertyDictionary[@"name"] = self.name;
-    propertyDictionary[@"type"] = self.type;
     propertyDictionary[@"value"] = self.number ?: self.value;
     return [propertyDictionary copy];
 }
